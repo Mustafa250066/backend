d829359fc0a7f24ab958bb0b625c5ea689da24de
Muhammad Mustafa <Mustafa250066@users.noreply.github.com>
2025-11-16 14:02:45 +0500

    update server.py

--- server.py
+++ server.py
@@ -33,7 +33,7 @@ app = FastAPI()
 # Enable CORS for frontend (React at localhost:3000)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","https://streamflix-mauve.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
